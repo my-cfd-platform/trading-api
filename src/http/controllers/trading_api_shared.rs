1fd9bfd9a74bb420949f0f86507b029ff3dd4d40
RsSvc <wonderhays@gmail.com>
2023-03-24 20:47:11 +0200

    fixed codes

--- src/http/controllers/trading_api_shared.rs
+++ src/http/controllers/trading_api_shared.rs
@@ -4,119 +4,99 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Copy, Deserialize, Serialize, Debug, MyHttpIntegerEnum)]
 pub enum ApiResponseCodes {
     #[http_enum_case(id="0"; value="0"; description="")]
-    DayOff,
-
+    Ok,
     #[http_enum_case(id="1"; value="1"; description="")]
-    MinOperationLotViolated,
-
+    DayOff,
     #[http_enum_case(id="2"; value="2"; description="")]
-    MaxOperationLotViolated,
-
-  
+    MinOperationLotViolated,
     #[http_enum_case(id="3"; value="3"; description="")]
-    MaxPositionByInstrumentViolated,
-
-    // /// <summary>
-    // /// Not enough balance to perform operation
-    // /// </summary>
+    MaxOperationLotViolated,
     #[http_enum_case(id="4"; value="4"; description="")]
-    InsufficientMargin,
-
-    // /// <summary>
-    // /// Bid or Ask is not found
-    // /// </summary>
+    MaxPositionByInstrumentViolated,
     #[http_enum_case(id="5"; value="5"; description="")]
-    NoLiquidity,
-
-    // /// <summary>
-    // /// Position not found to perform operation
-    // /// </summary>
+    InsufficientMargin,
     #[http_enum_case(id="6"; value="6"; description="")]
+    NoLiquidity,
+    #[http_enum_case(id="7"; value="7"; description="")]
     PositionNotFound,
 
     // /// <summary>
     // /// Take profit is to close to the market
     // /// </summary>
-    #[http_enum_case(id="7"; value="7"; description="")]
+    #[http_enum_case(id="8"; value="8"; description="")]
     TpIsTooClose,
 
     // /// <summary>
     // /// Sto loss is to close to the market
     // /// </summary>
-    #[http_enum_case(id="8"; value="8"; description="")]
+    #[http_enum_case(id="9"; value="9"; description="")]
     SlIsTooClose,
 
     // /// <summary>
     // /// Pending order is not found to perform operation
     // /// </summary>
-    #[http_enum_case(id="9"; value="9"; description="")]
+    #[http_enum_case(id="10"; value="10"; description="")]
     PendingOrderNotFound,
 
     // /// <summary>
     // /// Account not found
     // /// </summary>
-    #[http_enum_case(id="10"; value="10"; description="")]
+    #[http_enum_case(id="11"; value="11"; description="")]
     AccountNotFound,
 
     // /// <summary>
     // /// Instrument not found
     // /// </summary>
-    #[http_enum_case(id="11"; value="11"; description="")]
+    #[http_enum_case(id="12"; value="12"; description="")]
     InstrumentNotFound,
 
     // /// <summary>
     // /// Instrument can not be used at the moment
     // /// </summary>
-    #[http_enum_case(id="12"; value="12"; description="")]
+    #[http_enum_case(id="13"; value="13"; description="")]
     InstrumentCanNotBeUsed,
 
     // /// <summary>
     // /// OperationIsNotPossibleDuringSwap
     // /// </summary>
-    #[http_enum_case(id="13"; value="13"; description="")]
+    #[http_enum_case(id="14"; value="14"; description="")]
     OperationIsNotPossibleDuringSwap,
 
     // /// <summary>
     // ///
     // /// </summary>
-    #[http_enum_case(id="14"; value="14"; description="")]
+    #[http_enum_case(id="15"; value="15"; description="")]
     MaxAmountPendingOrders,
 
     // /// <summary>
     // /// Technical Error
     // /// </summary>
-    #[http_enum_case(id="15"; value="15"; description="")]
+    #[http_enum_case(id="16"; value="16"; description="")]
     TechnicalError,
 
     // /// <summary>
     // /// Multiplier Not Found
     // /// </summary>
-    #[http_enum_case(id="16"; value="16"; description="")]
+    #[http_enum_case(id="17"; value="17"; description="")]
     MultiplierNotFound,
 
     // /// <summary>
     // /// Instrument can not be used at the moment
     // /// </summary>
-    #[http_enum_case(id="17"; value="17"; description="")]
+    #[http_enum_case(id="18"; value="18"; description="")]
     MaximumAmountOfDemoAccount,
 
     // /// <summary>
     // /// Trading Disabled
     // /// </summary>
-    #[http_enum_case(id="18"; value="18"; description="")]
+    #[http_enum_case(id="19"; value="19"; description="")]
     TradingDisabled,
 
     // /// <summary>
     // /// Max Open Positions Amount
     // /// </summary>
-    #[http_enum_case(id="19"; value="19"; description="")]
-    MaxOpenPositionsAmount,
-
-    // /// <summary>
-    // /// Ok
-    // /// </summary>
     #[http_enum_case(id="20"; value="20"; description="")]
-    Ok,
+    MaxOpenPositionsAmount,
 
     #[http_enum_case(id="21"; value="21"; description="")]
     NotEnoughBalance,
